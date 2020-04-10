3aeaa5d219a2956db1b5f1f20bb9ab9498aad9f3
Romain <romain.goasdoue@epitech.eu>
2020-04-10 16:42:12 +0200

    linker.ld, compile and run kernel

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,9 +3,9 @@
 
 //Check if my x86-elf cross compiler is used correctly
 #if defined(__linux__)
-    #error "This code must be compiled with a cross-compiler"
+	#error "This code must be compiled with a cross-compiler"
 #elif !defined(__i386__)
-    #error "This code must be compiled with an x86 compiler"
+	#error "This code must be compiled with an x86 compiler"
 #endif
 
 //x86's VGA textmode buffer. Displaying text goes by writing data in this memory location
@@ -22,52 +22,53 @@ uint8_t             term_color = 0x0F; //black background & white foreground
 //Initiate term by clearing it
 void    term_init()
 {
-    //Clear textmode buffer
-    for (int col = 0; col < VGA_COLS; col++) {
-        for (int row = 0; row < VGA_ROWS; row++) {
-            //VGA textmode buffer size is (VGA_COLS * VGA_ROWS)
-            const size_t index = (VGA_COLS * row) + col;
+	//Clear textmode buffer
+	for (int col = 0; col < VGA_COLS; col++) {
+		for (int row = 0; row < VGA_ROWS; row++) {
+			//VGA textmode buffer size is (VGA_COLS * VGA_ROWS)
+			const size_t index = (VGA_COLS * row) + col;
 
-            //Entries in VGA buffer are BBBBFFFFCCCCCCCC:
-            //  -B: background color
-            //  -F: foreground color
-            //  -C: ASCII character
-            vga_buffer[index] = ((uint16_t)term_col << 8) | ' '; //Set the character to blank
-        }
-    }
+			//Entries in VGA buffer are BBBBFFFFCCCCCCCC:
+			//  -B: background color
+			//  -F: foreground color
+			//  -C: ASCII character
+			vga_buffer[index] = ((uint16_t)term_col << 8) | ' '; //Set the character to blank
+		}
+	}
 }
 
 void    term_putc(char c)
 {
-    if (c == '\n') { //'\n' return a column 0 and increment the row
-        term_col = 0;
-        term_row++;
-    } else { //Put char in buffer and increment column
-        const size_t index = (VGA_COLS * term_row) + term_col;
-        vga_buffer[index] = ((uint16_t)term_color << 8) | c;
-        term_col++;
-    }
+	if (c == '\n') { //'\n' return a column 0 and increment the row
+		term_col = 0;
+		term_row++;
+	} else { //Put char in buffer and increment column
+		const size_t index = (VGA_COLS * term_row) + term_col;
+		vga_buffer[index] = ((uint16_t)term_color << 8) | c;
+		term_col++;
+	}
 
-    //If we go beyond the number of columns
-    if (term_col >= VGA_COLS) {
-        term_col = 0;
-        term_row++;
-    }
-    //If we go beyond the number of rows
-    if (term_row >= VGA_ROWS) {
-        term_row = term_row = 0;
-    }
+	//If we go beyond the number of columns
+	if (term_col >= VGA_COLS) {
+		term_col = 0;
+		term_row++;
+	}
+	//If we go beyond the number of rows
+	if (term_row >= VGA_ROWS) {
+		term_row = term_row = 0;
+	}
 }
 
-void    term_print(const char *str) {
-    for (int i = 0; str[i] != '\0'; i++)
-        term_putc(str[i]);
+void    term_print(const char *str)
+{
+	for (size_t i = 0; str[i] != '\0'; i++)
+		term_putc(str[i]);
 }
 
 void kernel_main()
 {
-    term_init();
+	term_init();
 
-    term_print("Hello, World!\n");
-    term_print("Welcome to RogOS's kernel.\n");
+	term_print("Hello, World!\n");
+	term_print("Welcome to RogOS's kernel.\n");
 }
--- kernel/start.s
+++ kernel/start.s
@@ -1,36 +1,37 @@
-;External kernel_main because we call the main from kernel.c
-.extern kernel_main
 
-;Start is global so the linker knows where to find it
+.extern kernel_main //External kernel_main because we call the main from kernel.c
+
+//Start is global so the linker knows where to find it
 .global start
 
-;Infos about the kernel for GRUB, we use a standard header called 'Multiboot'
-.set MB_MAGIC, 0x1BADB002   ;'Magic' constant used by GRUB to detect kernel's location
-.set MB_FLAGS, (1 << 0) | (1 << 1) ; instructions for GRUB: 1) load modules on page boundaries 2) provide memory map
-.set MB_CHECKSUM, (0 - (MB_MAGIC + MB_FLAGS)) ; checksum of previous values
+//Infos about the kernel for GRUB, we use a standard header called 'Multiboot'
+.set MB_MAGIC, 0x1BADB002   //'Magic' constant used by GRUB to detect kernel's location
+.set MB_FLAGS, (1 << 0) | (1 << 1) // instructions for GRUB: 1) load modules on page boundaries 2) provide memory map
+.set MB_CHECKSUM, (0 - (MB_MAGIC + MB_FLAGS)) // checksum of previous values
 
-;Our 'multiboot' header
+//Our 'multiboot' header
 .section .multiboot
-    .align 4 ;make sure data is aligned on 4 bytes (or a multiple)
-    .long MB_MAGIC
-    .long MB_FLAGS
-    .long MB_CHECKSUM
+	.align 4 //make sure data is aligned on 4 bytes (or a multiple)
+	.long MB_MAGIC
+	.long MB_FLAGS
+	.long MB_CHECKSUM
 
-;Data initiliazed to 0 when kernel is loaded
+//Data initiliazed to 0 when kernel is loaded
 .section .bss
-    ;We create a stack of 4096 bytes for our C code
-    .align 16
-    stack_bottom:
-        .skip 4096
-    stack_top:
+	//We create a stack of 4096 bytes for our C code
+	.align 16
+	stack_bottom:
+		.skip 4096
+	stack_top:
 
-;The code run when the kernel is loaded
+
+//The code run when the kernel is loaded
 .section .text
-    start: ;First code run in the kernel
-        mov %stack_top, %esp ;Setup stack for our C code, then C environment is ready
-        call kernel_main ;Calling our kernel main function
+	start: //First code run in the kernel
+		mov $stack_top, %esp //Setup stack for our C code, then C environment is ready
+		call kernel_main //Calling our kernel main function
 
-    hang: ;If the kernel main returns anything
-        cli ;Disable CPU interrupts
-        hlt ;Halt the CPU
-        jmp hang ;Try again
+	hang: //If the kernel main returns anything
+		cli //Disable CPU interrupts
+		hlt //Halt the CPU
+		jmp hang //Try again
